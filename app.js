0bcc2f307725520c1f7ed045d00759f6e0389fdb
timmy304681 <timmy304681@gmail.com>
2022-10-24 15:20:27 +0800

    feat: first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// Express Initialization
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+require('dotenv').config();
+const { PORT, API_VERSION } = process.env;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// CORS allow all
+app.use(cors());
+
+// API routes
+app.use('/api/' + API_VERSION, [
+  require('./routes/shortenURL1_route'),
+  require('./routes/shortenURL2_route'),
+  require('./routes/shortenURL3_route'),
+]);
+
+// Error handling
+app.use(function (err, req, res, next) {
+  console.log(err);
+  res.status(500).send('Internal Server Error');
+});
+
+//set port to 3000
+app.listen(SERVER_PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
