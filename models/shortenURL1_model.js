cb81e06a8f74494e7eea5b757ad49eaf5fef94ef
david31009 <david31009@gmail.com>
2022-10-24 21:56:10 +0800

    fix: getLongURL from different db replica

--- models/shortenURL1_model.js
+++ models/shortenURL1_model.js
@@ -1,13 +1,16 @@
 // require('dotenv').config({ path: '../.env' });
 // const pool = require('./mysqlcon');
-const pool = require('./mysqlconf');
+const poolWrite = require('./mysqlconf').dbWrite;
+const poolRead1 = require('./mysqlconf').dbRead1;
+const poolRead2 = require('./mysqlconf').dbRead2;
+const poolRead3 = require('./mysqlconf').dbRead3;
 const rand = require('random-key');
 
 const addShortUrl = async (longUrl) => {
   const getRandom = rand.generate(6);
   // 組成七碼隨機 url
   const shortUrl = process.env.EC2_NUM + getRandom;
-  const conn = await pool.getConnection();
+  const conn = await poolWrite.getConnection();
   try {
     await conn.query('START TRANSACTION');
     let [resCheckShortUrl] = await conn.query(
@@ -24,7 +27,7 @@ const addShortUrl = async (longUrl) => {
         [shortUrl]
       );
     }
-    const [result] = await conn.query(
+    await conn.query(
       'INSERT INTO `url_info` (short_url, long_url) VALUES (?,?)',
       [shortUrl, longUrl]
     );
@@ -38,12 +41,28 @@ const addShortUrl = async (longUrl) => {
   }
 };
 
-const getLongUrl = async (shortUrl) => {
-  const [result] = await pool.query(
+const getLongUrl1 = async (shortUrl) => {
+  const [result] = await poolRead1.query(
     `SELECT * FROM url_info WHERE short_url = ?`,
     [shortUrl]
   );
   return result[0].long_url;
 };
 
-module.exports = { addShortUrl, getLongUrl };
+const getLongUrl2 = async (shortUrl) => {
+  const [result] = await poolRead2.query(
+    `SELECT * FROM url_info WHERE short_url = ?`,
+    [shortUrl]
+  );
+  return result[0].long_url;
+};
+
+const getLongUrl3 = async (shortUrl) => {
+  const [result] = await poolRead3.query(
+    `SELECT * FROM url_info WHERE short_url = ?`,
+    [shortUrl]
+  );
+  return result[0].long_url;
+};
+
+module.exports = { addShortUrl, getLongUrl1, getLongUrl2, getLongUrl3 };
