19302b323adb368c780c23f30581fb6a76cd3400
david31009 <david31009@gmail.com>
2022-10-24 21:25:06 +0800

    feat: get short url and return long url

--- models/shortenURL1_model.js
+++ models/shortenURL1_model.js
@@ -1,6 +1,9 @@
+// require('dotenv').config({ path: '../.env' });
+// const pool = require('./mysqlcon');
 const pool = require('./mysqlconf');
 const rand = require('random-key');
 
+const longUrl = 'google.com';
 const addShortUrl = async (longUrl) => {
   const getRandom = rand.generate(6);
   // 組成七碼隨機 url
@@ -26,8 +29,8 @@ const addShortUrl = async (longUrl) => {
       'INSERT INTO `url_info` (short_url, long_url) VALUES (?,?)',
       [shortUrl, longUrl]
     );
-    console.log(result);
     await conn.query('COMMIT');
+    return shortUrl;
   } catch (error) {
     await conn.query('ROLLBACK');
     return { error };
@@ -36,4 +39,12 @@ const addShortUrl = async (longUrl) => {
   }
 };
 
-module.exports = { addShortUrl };
+const getLongUrl = async (shortUrl) => {
+  const [result] = await pool.query(
+    `SELECT * FROM url_info WHERE short_url = ?`,
+    [shortUrl]
+  );
+  return result[0].long_url;
+};
+
+module.exports = { addShortUrl, getLongUrl };
