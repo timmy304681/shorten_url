f3311b197111aa4b0e503e8f077675f66bbe8cb3
timmy304681 <timmy304681@gmail.com>
2022-10-24 20:58:38 +0800

    feat: add shortUR1_controller

---
+++ routes/shortenURL1_route.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { getURL, postURL } = require('../controllers/shortenURL1_controller');
+
+router.get('/v1/:shortenURL', getURL);
+
+router.post('/data/shortenURL1', postURL);
